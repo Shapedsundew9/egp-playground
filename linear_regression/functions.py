900eb24d1b9effdf9b2b0c31b28d27edb18eb18d
Shaped Sundew <16618209+Shapedsundew9@users.noreply.github.com>
2023-09-10 11:53:07 -0400

    Latest

--- linear_regression/functions.py
+++ linear_regression/functions.py
@@ -27,19 +27,63 @@ _LOG_DEBUG: bool = _logger.isEnabledFor(DEBUG)
 # 'threshold' is the fitness threshold that must be met or exceeded to move to the next difficulty level
 # 'data' is the generated samples & expected results for the given difficulty level
 PARAMS = {
-    'trivial': {'seed': 1, 'scale': 10, 'N': 10, 'noise': 0.01, 'samples': 10, 'threshold': 0.500, 'data': None},
-    'easy': {'seed': 2, 'scale': 100, 'N': 100, 'noise': 0.10, 'samples': 100, 'threshold': 0.850, 'data': None},
-    'medium': {'seed': 3, 'scale': 1000, 'N': 1000, 'noise': 0.25, 'samples': 200, 'threshold': 0.950, 'data': None},
-    'hard': {'seed': 4, 'scale': 10000, 'N': 5000, 'noise': 0.50, 'samples': 300, 'threshold': 0.990, 'data': None},
-    'extreme': {'seed': 5, 'scale': 100000, 'N': 10000, 'noise': 1.00, 'samples': 1000, 'threshold': 0.999, 'data': None}
+    "trivial": {
+        "seed": 1,
+        "scale": 10,
+        "N": 10,
+        "noise": 0.01,
+        "samples": 10,
+        "threshold": 0.500,
+        "data": None,
+    },
+    "easy": {
+        "seed": 2,
+        "scale": 100,
+        "N": 100,
+        "noise": 0.10,
+        "samples": 100,
+        "threshold": 0.850,
+        "data": None,
+    },
+    "medium": {
+        "seed": 3,
+        "scale": 1000,
+        "N": 1000,
+        "noise": 0.25,
+        "samples": 200,
+        "threshold": 0.950,
+        "data": None,
+    },
+    "hard": {
+        "seed": 4,
+        "scale": 10000,
+        "N": 5000,
+        "noise": 0.50,
+        "samples": 300,
+        "threshold": 0.990,
+        "data": None,
+    },
+    "extreme": {
+        "seed": 5,
+        "scale": 100000,
+        "N": 10000,
+        "noise": 1.00,
+        "samples": 1000,
+        "threshold": 0.999,
+        "data": None,
+    },
 }
+
 
 def preload_function():
     """In this case we don't need to preload anything.
     Data is generated.
     """
 
-def generate_x(scale: real, N: int, rng: Generator, narrow: bool = False) -> tuple[array]:
+
+def generate_x(
+    scale: real, N: int, rng: Generator, narrow: bool = False
+) -> tuple[array]:
     """Generate the N x values for the linear regression model.
 
     -scale <= x <= scale
@@ -72,34 +116,39 @@ def add_noise(x: array, y: array, noise: real, rng: Generator) -> tuple[array, a
 def sample(num: int, difficulty: str) -> tuple[array, array, real, real]:
     """Lazy generation of data for the given difficulty level and sample number."""
     param_set = PARAMS[difficulty]
-    if param_set['data'] is None:
-        rng = default_rng(param_set['seed'])
-        _logger.info(f'Generating data for {difficulty} data samples...')
-        param_set['data'] = {
-            'x': empty((param_set['samples'], param_set['N']), dtype=real),
-            'y': empty((param_set['samples'], param_set['N']), dtype=real),
-            'f_m': empty((param_set['samples'],), dtype=real),
-            'f_c': empty((param_set['samples'],), dtype=real),
+    if param_set["data"] is None:
+        rng = default_rng(param_set["seed"])
+        _logger.info(f"Generating data for {difficulty} data samples...")
+        param_set["data"] = {
+            "x": empty((param_set["samples"], param_set["N"]), dtype=real),
+            "y": empty((param_set["samples"], param_set["N"]), dtype=real),
+            "f_m": empty((param_set["samples"],), dtype=real),
+            "f_c": empty((param_set["samples"],), dtype=real),
         }
-        for sample_num in range(param_set['samples']):
+        for sample_num in range(param_set["samples"]):
             m = tan(rng.uniform(-pi, pi))
-            c = rng.uniform(-param_set['scale'], param_set['scale'])
-            x = generate_x(param_set['scale'], param_set['N'], rng)
+            c = rng.uniform(-param_set["scale"], param_set["scale"])
+            x = generate_x(param_set["scale"], param_set["N"], rng)
             y = m * x + c
-            x, y = add_noise(x, y, param_set['noise'], rng)
+            x, y = add_noise(x, y, param_set["noise"], rng)
             f_m, f_c, _, _, _ = linregress(x, y)
-            param_set['data']['x'][sample_num] = x
-            param_set['data']['y'][sample_num] = y
-            param_set['data']['f_m'][sample_num] = f_m
-            param_set['data']['f_c'][sample_num] = f_c
-        _logger.info(f'Data for {difficulty} data samples generated.')
-    return param_set['data']['x'][num], param_set['data']['y'][num], param_set['data']['f_m'][num], param_set['data']['f_c'][num]
+            param_set["data"]["x"][sample_num] = x
+            param_set["data"]["y"][sample_num] = y
+            param_set["data"]["f_m"][sample_num] = f_m
+            param_set["data"]["f_c"][sample_num] = f_c
+        _logger.info(f"Data for {difficulty} data samples generated.")
+    return (
+        param_set["data"]["x"][num],
+        param_set["data"]["y"][num],
+        param_set["data"]["f_m"][num],
+        param_set["data"]["f_c"][num],
+    )
 
 
 def fitness_function(individual: Callable[[array, array], tuple[real, real]]) -> real:
     """The individual is a function that takes an array of x values and an array of
      y values and returns a tuple of m and c.
-     
+
     The fitness function is the sum of the squared errors between the evolved m & c
     and that found by scipy linregress.
 
@@ -111,18 +160,18 @@ def fitness_function(individual: Callable[[array, array], tuple[real, real]]) ->
     sample_fitness = []
     for difficulty, param_set in PARAMS.items():
         if _LOG_DEBUG:
-            _logger.debug(f'Running fitness_function with {difficulty} parameters')
-        for sample_num in range(param_set['samples']):
+            _logger.debug(f"Running fitness_function with {difficulty} parameters")
+        for sample_num in range(param_set["samples"]):
             x, y, f_m, f_c = sample(sample_num, difficulty)
             i_m, i_c = individual(x, y)
             sample_fitness.append(1.0 - ((i_m - f_m) ** 2 + (i_c - f_c) ** 2) / 2.0)
-        fitness = 1.0 - mean((1.0 - sample_fitness)**2)
-        if fitness < param_set['threshold']:
+        fitness = 1.0 - mean((1.0 - sample_fitness) ** 2)
+        if fitness < param_set["threshold"]:
             break
     return fitness
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Do some characterization of the data sets
     # For each difficulty level:
     #    1. Plot the distribution of graident angles
--- number_tree/functions.py
+++ number_tree/functions.py
@@ -44,15 +44,20 @@ def preload_function() -> None:
 def fitness_function(individual: Callable[[int, int], int]) -> real:
     """The fitness function is about the 'correctness' of the solution.
     See the survivability function for what constitutes the best solution.
-    
+
     0.0 <= fitness <= 1.0
 
     For this problem the fitness is 1 - the square of the error in Z over Z**2.
     """
-    return max(1.0 - ((Z - individual(X, Y)) / Z)**2, 1.0)
+    return max(1.0 - ((Z - individual(X, Y)) / Z) ** 2, 1.0)
 
 
-def survivability_function(populous: population) -> tuple[ndarray[real], ndarray[bool]] | tuple[ndarray[int64], ndarray[real], ndarray[bool]]:
+def survivability_function(
+    populous: population,
+) -> (
+    tuple[ndarray[real], ndarray[bool]]
+    | tuple[ndarray[int64], ndarray[real], ndarray[bool]]
+):
     """The survivability function is used to determine which solutions continue to be evolved & which will be deleted.
     It is called after each epoch and has 2 ways to define the next epoch:
         1. Set 'active' to True for the individuals to continue to be evolved. Survivability is ignored.
@@ -83,18 +88,21 @@ def survivability_function(populous: population) -> tuple[ndarray[real], ndarray
 
 
 def random_solution(x: int, y: int, limit: int = 4 * Z) -> list[int]:
-    """Generate a random solution to the problem.
-    """
+    """Generate a random solution to the problem."""
     path = ([0], [0])
     z = 0
     while z != Z:
         path[0].append(path[0][-1])
         path[1].append(path[1][-1])
         match randint(0, 3):
-            case 0: path[0][-1] += x
-            case 1: path[0][-1] -= x
-            case 2: path[1][-1] += y
-            case 3: path[1][-1] -= y
+            case 0:
+                path[0][-1] += x
+            case 1:
+                path[0][-1] -= x
+            case 2:
+                path[1][-1] += y
+            case 3:
+                path[1][-1] -= y
         z = path[0][-1] + path[1][-1]
         if z > limit or z < -limit:
             path[0].pop()
@@ -103,12 +111,14 @@ def random_solution(x: int, y: int, limit: int = 4 * Z) -> list[int]:
 
 
 # Run as a script for testing
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Set up problem space
     seed(1)
     z_limit = 2 * Z
     boundary = int(2 * Z / min(X, Y)) + 1
-    x_space, y_space = meshgrid(arange(-boundary, boundary + 1) * X, arange(-boundary, boundary + 1) * Y)
+    x_space, y_space = meshgrid(
+        arange(-boundary, boundary + 1) * X, arange(-boundary, boundary + 1) * Y
+    )
     dz_space = sqrt(abs(x_space + y_space - Z))
 
     # Generate a random path to the solution
@@ -117,21 +127,39 @@ if __name__ == '__main__':
     # Plot the random path
     fig, ax = plt.subplots()
     fig.set_size_inches(12, 10)
-    c = ax.pcolormesh(x_space, y_space, dz_space, cmap='RdBu', vmin=0, vmax=dz_space.max())
-    ax.set_title(f'A Random Path to Solution. X={X}, Y={Y}, Z={Z}, length={len(random_path[0]) - 1}.')
+    c = ax.pcolormesh(
+        x_space, y_space, dz_space, cmap="RdBu", vmin=0, vmax=dz_space.max()
+    )
+    ax.set_title(
+        f"A Random Path to Solution. X={X}, Y={Y}, Z={Z}, length={len(random_path[0]) - 1}."
+    )
     ax.axis([x_space.min(), x_space.max(), y_space.min(), y_space.max()])
     fig.colorbar(c, ax=ax)
-    ax.plot(random_path[0], random_path[1], linewidth=2, color='black', label='random path')
-    ax.plot([0], [0], marker='x', markersize=5, color='green', label='origin')
-    ax.plot(random_path[0][-1], random_path[1][-1], markersize=5, marker='o', color='yellow', label='solution')
-    plt.savefig('random_path.png')
+    ax.plot(
+        random_path[0], random_path[1], linewidth=2, color="black", label="random path"
+    )
+    ax.plot([0], [0], marker="x", markersize=5, color="green", label="origin")
+    ax.plot(
+        random_path[0][-1],
+        random_path[1][-1],
+        markersize=5,
+        marker="o",
+        color="yellow",
+        label="solution",
+    )
+    plt.savefig("random_path.png")
 
     # Generate a population of random solutions
-    path_lengths = [len(random_solution(X, Y, z_limit)[0]) for _ in trange(10000, desc='Generating random paths')]
+    path_lengths = [
+        len(random_solution(X, Y, z_limit)[0])
+        for _ in trange(10000, desc="Generating random paths")
+    ]
     fig, ax = plt.subplots()
     fig.set_size_inches(12, 10)
-    ax.hist(path_lengths, bins=range(0, max(path_lengths) + 10, 10), color='blue')
-    ax.set_title(f'Random Path Length Distribution. X={X}, Y={Y}, Z={Z}. 10k samples, bin width = 10.')
-    ax.set_xlabel('Path length')
-    ax.set_ylabel('Frequency')
-    plt.savefig('random_path_distribution.png')
+    ax.hist(path_lengths, bins=range(0, max(path_lengths) + 10, 10), color="blue")
+    ax.set_title(
+        f"Random Path Length Distribution. X={X}, Y={Y}, Z={Z}. 10k samples, bin width = 10."
+    )
+    ax.set_xlabel("Path length")
+    ax.set_ylabel("Frequency")
+    plt.savefig("random_path_distribution.png")
